9aa890accf893e5f46de3526ea92c09308d6b23f
francescotrudu <francescotrudu84@gmail.com>
2023-02-04 16:42:27 +0100

    Add istances for genres

--- db/db.php
+++ db/db.php
@@ -8,17 +8,18 @@ $harry_plot2 = 'Il secondo anno ad Hogwarts si rivela denso di pericoli per Harr
 $batman_plot = 'Traumatizzato dalla morte dei genitori, il giovane Bruce Wayne intraprende un addestramento fisico e psicologico con il suo mentore per prepararsi alla lotta contro il male nella città di Gotham City.';
 $spiderman_plot = 'Il morso di un ragno mutante dona degli incredibili, e inaspettati, poteri al giovane Peter Parker, che li usa nella guerra contro un minaccioso nemico.';
 
-//genres
-$action = new Genre('Action');
-$thriller = new Genre('Thriller');
-$adventure = new Genre('Adventure');
-$fantasy = new Genre('Fantasy');
-$family = new Genre('Family');
-
 
 $movies = [
-    $harry_potter_1 = new Movie('Harry Potter', 'Daniel Radcliffe', $harry_plot1, [$adventure, $fantasy, $family], false, 'https://www.warnerbros.it/sites/default/files/HP1_MOVIE_V_DD_KA_TT_1400x2100_300dpi_IT.jpg'),
-    $harry_potter_2 = new Movie('Harry Potter 2', 'Daniel Radcliffe', $harry_plot2, [$adventure, $fantasy, $family], false, 'https://picfiles.alphacoders.com/232/thumb-232115.jpg'),
-    $batman = new Movie('Batman begins', 'Christian Bale', $batman_plot, [$action], true, 'https://m.media-amazon.com/images/I/51BZHCbJ-QL._SY445_.jpg'),
-    $spiderman = new Movie('Spiderman', 'Tobey Maguire', $spiderman_plot, [$action, $fantasy], false, 'https://m.media-amazon.com/images/I/81jRVXCIGeL.jpg')
+    $harry_potter_1 = new Movie('Harry Potter', 'Daniel Radcliffe', $harry_plot1, new Genre(['Adventure', 'Family']), false, 'https://www.warnerbros.it/sites/default/files/HP1_MOVIE_V_DD_KA_TT_1400x2100_300dpi_IT.jpg'),
+    $harry_potter_2 = new Movie('Harry Potter 2', 'Daniel Radcliffe', $harry_plot2, new Genre(['Family', 'Fantasy']), false, 'https://picfiles.alphacoders.com/232/thumb-232115.jpg'),
+    $batman = new Movie('Batman begins', 'Christian Bale', $batman_plot, new Genre(['Adventure', 'Fantasy']), true, 'https://m.media-amazon.com/images/I/51BZHCbJ-QL._SY445_.jpg'),
+    $spiderman = new Movie('Spiderman', 'Tobey Maguire', $spiderman_plot, new Genre(['Adventure', 'Fantasy', 'Family']), false, 'https://m.media-amazon.com/images/I/81jRVXCIGeL.jpg')
 ];
+
+// $obj_movies = [];
+
+// foreach ($movies as $movie) {
+//     $obj_movies[] = get_object_vars($movie);
+// }
+
+// var_dump($obj_movies);
